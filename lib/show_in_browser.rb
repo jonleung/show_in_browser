3f28d7e71bd03a12e20f382e432996d31e36c266
Jonathan Leung <naruto137@gmail.com>
2012-12-27 13:46:08 -0500

    First Commit

---
+++ lib/show_in_browser.rb
@@ -0,0 +1,24 @@
+class ShowInBrowser
+  class << self
+
+    def show(html)
+      escaped_html = shell_escape(html)
+      `f="/tmp/browser.html"
+       rm $f
+       echo "#{html}" >> $f
+       open $f
+      `
+      puts escaped_html
+    end
+
+    def shell_escape(str)
+      return "''" if str.empty?
+
+      str = str.dup
+      str.gsub!(/([^A-Za-z0-9_\-.,:\/@\n])/n, "\\\\\\1")
+      str.gsub!(/\n/, "'\n'")
+      return str
+    end
+
+  end
+end
---
+++ show_in_browser.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'show_in_browser'
+  s.version     = '0.0.1'
+  s.date        = '2012-12-27'
+  s.summary     = "Opens an html string in your default browser."
+  s.description = "Opens arbitrary text in your browser"
+  s.authors     = ["Jonathan Leung"]
+  s.email       = 'me@jonl.org'
+  s.files       = ["lib/show_in_browser.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/show_in_browser'
+end
